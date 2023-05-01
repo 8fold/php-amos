c5306f869a35257d30f59983ac5abb0b0b4ec7fb
Josh Bruce <josh@8fold.pro>
2023-04-30 20:17:01 -0400

    fix: Duplicate page title on home pages

--- tests/SiteTest.php
+++ tests/SiteTest.php
@@ -12,6 +12,39 @@ class SiteTest extends TestCase
     /**
      * @test
      */
+    public function page_title_does_not_duplicate_root(): void
+    {
+        $expected = ['Root test content'];
+
+        $result = Site::init(
+            'https://ex.ample',
+            __DIR__ . '/test-content'
+        )->titles('/');
+
+        $this->assertSame(
+            $expected,
+            $result
+        );
+
+        $expected = [
+            'Deeper page',
+            'Root test content'
+        ];
+
+        $result = Site::init(
+            'https://ex.ample',
+            __DIR__ . '/test-content'
+        )->titles('/deeper-page');
+
+        $this->assertSame(
+            $expected,
+            $result
+        );
+    }
+
+    /**
+     * @test
+     */
     public function can_initialize_site(): void
     {
         $this->assertNotNull(
