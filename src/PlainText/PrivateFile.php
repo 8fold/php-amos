92eadb88e7f5c9d489b03426b48ef0e022a12621
Josh Bruce <josh@8fold.pro>
2023-05-17 17:57:23 -0400

    create: PrivateFile - same as PrivateJson

---
+++ src/PlainText/PrivateFile.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Eightfold\Amos\PlainText;
+
+use Eightfold\Amos\FileSystem\Directories\Root;
+use Eightfold\Amos\FileSystem\Files\PrivateFile;
+
+use Eightfold\Amos\Php\Interfaces\Falsifiable;
+use Eightfold\Amos\Php\Interfaces\Stringable;
+
+final class PrivateFile
+{
+    public static function inRoot(
+        Root $root,
+        string $filename,
+        string $at = ''
+    ): self {
+        return new self(
+            PrivateFile::inRoot($root, $filename, $at)
+        );
+    }
+
+    final private function __construct(
+        private readonly PrivateFile $privateFile
+    ) {
+    }
+
+    public function notFound(): bool
+    {
+        return $this->privateFile->notFound();
+    }
+
+    public function isFile(): bool
+    {
+        return $this->privateFile->isFile();
+    }
+
+    public function toBool(): bool
+    {
+        return $this->privateFile->toBool();
+    }
+
+    public function toString(): string
+    {
+        if ($this->notFound()) {
+            return '';
+        }
+
+        $content = file_get_contents($this->privateFile->toString());
+        if ($content === false) {
+            return '';
+        }
+        return $content;
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+}
