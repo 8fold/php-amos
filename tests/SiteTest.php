e1f30f2154efe65a7504f9abd53540328854cb46
Josh Bruce <josh@8fold.pro>
2023-12-19 17:19:42 -0500

    add: Level 4 test content

--- tests/SiteTest.php
+++ tests/SiteTest.php
@@ -89,6 +89,28 @@ class SiteTest extends TestCase
 
     /**
      * @test
+     * @group current
+     */
+    public function can_build_breadcrumbs_l4(): void
+    {
+        $expected = [
+            '/' => 'Root test content',
+            '/l1-page/'                         => 'L1 page',
+            '/l1-page/l2-page/'                 => 'L2 page',
+            '/l1-page/l2-page/l3-page/'         => 'L3 page',
+            '/l1-page/l2-page/l3-page/l4-page/' => 'L4 page'
+        ];
+
+        $result = $this->site()->breadcrumbs('/l1-page/l2-page/l3-page/l4-page');
+
+        $this->assertSame(
+            $expected,
+            $result
+        );
+    }
+
+    /**
+     * @test
      */
     public function has_public_meta(): void
     {
