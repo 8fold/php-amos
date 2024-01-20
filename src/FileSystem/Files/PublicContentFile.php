3267e54f511e172a31280dbfa98fe73979f9955e
Josh Bruce <josh@8fold.pro>
2024-01-20 11:06:36 -0500

    add: PublicContentFile can accept Path

--- src/FileSystem/Files/PublicContentFile.php
+++ src/FileSystem/Files/PublicContentFile.php
@@ -17,15 +17,21 @@ final class PublicContentFile implements Findable, Stringable
 {
     private const FILENAME = 'content.md';
 
-    public static function inRoot(Root $root, string $at = ''): self
+    public static function inRoot(
+        Root $root,
+        string|Path $at = ''
+    ): self
     {
         return self::inPublicRoot($root->publicRoot(), $at);
     }
 
-    public static function inPublicRoot(PublicRoot $root, string $at = ''): self
-    {
-        $at = Path::fromString($at)->toString();
-
+    public static function inPublicRoot(
+        PublicRoot $root,
+        string|Path $at = ''
+    ): self {
+        if (is_string($at)) {
+            $at = Path::fromString($at);
+        }
         return new self(
             PublicFile::inPublicRoot($root, self::FILENAME, $at)
         );
--- tests/FileSystem/Files/PublicContentFileTest.php
+++ tests/FileSystem/Files/PublicContentFileTest.php
@@ -7,10 +7,75 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 use Eightfold\Amos\FileSystem\Files\PublicContentFile;
 
+use Eightfold\Amos\FileSystem\Path;
+
 use Eightfold\Amos\FileSystem\Directories\Root;
 
 class PublicContentFileTest extends BaseTestCase
 {
+    /**
+     * @test
+     */
+    public function can_check_existence_using_path(): void
+    {
+        $root = Root::fromString(
+            __DIR__ .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . '..' .
+            DIRECTORY_SEPARATOR . 'test-content');
+
+        $sut = PublicContentFile::inRoot($root);
+
+        $this->assertNotNull(
+            $sut
+        );
+
+        $result = $sut->toBool();
+
+        $this->assertTrue(
+            $result
+        );
+
+        $result = $sut->isFile();
+
+        $this->assertTrue(
+            $result
+        );
+
+        $result = $sut->notFound();
+
+        $this->assertFalse(
+            $result
+        );
+
+        $sut = PublicContentFile::inRoot(
+            $root,
+            Path::fromString('l1-page')
+        );
+
+        $this->assertNotNull(
+            $sut
+        );
+
+        $result = $sut->toBool();
+
+        $this->assertFalse(
+            $result
+        );
+
+        $result = $sut->isFile();
+
+        $this->assertFalse(
+            $result
+        );
+
+        $result = $sut->notFound();
+
+        $this->assertTrue(
+            $result
+        );
+    }
+
     /**
      * @test
      */
